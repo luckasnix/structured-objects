b10e78ba2f7451db93bc57218de8ff83529e2e4a
Kasnix <luc.kasnix@gmail.com>
2024-01-13 16:57:44 -0300

    chore: configure project for library building

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,23 @@
+import { resolve } from 'path';
 import { defineConfig } from 'vitest/config';
+import dts from 'vite-plugin-dts';
 
 export default defineConfig({
   test: {
     watch: false,
   },
+  build: {
+    lib: {
+      entry: resolve(__dirname, 'src/main.ts'),
+      name: 'ObjectGraph',
+      formats: ['es'],
+      fileName: 'main',
+    },
+  },
+  plugins: [
+    dts({
+      include: ['src'],
+      exclude: ['src/**/*.(test|mock).ts'],
+    })
+  ],
 });
