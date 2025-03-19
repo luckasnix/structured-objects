929ba4b7ef2379d217cc173865ead1bf6173f856
Kasnix <luc.kasnix@gmail.com>
2025-03-19 10:46:51 -0300

    refactor: simplify variable names

--- tests/object-graph.test.ts
+++ tests/object-graph.test.ts
@@ -11,69 +11,69 @@ import { ObjectGraph } from "../src/object-graph";
 describe("length", () => {
   test("gets the length of the object graph", () => {
     const shirtToAdd: Shirt = { sku: "9", color: "orange", size: "small" };
-    const shirtsObjectGraph = new ObjectGraph<Shirt>(shirtsMock, (shirt) => shirt.sku);
+    const shirtsGraph = new ObjectGraph<Shirt>(shirtsMock, (shirt) => shirt.sku);
 
-    expect(shirtsObjectGraph.length).toBe(8);
+    expect(shirtsGraph.length).toBe(8);
 
-    shirtsObjectGraph.add(shirtToAdd);
+    shirtsGraph.add(shirtToAdd);
 
-    expect(shirtsObjectGraph.length).toBe(9);
+    expect(shirtsGraph.length).toBe(9);
   });
 });
 
 describe("size", () => {
   test("gets the size of the object graph", () => {
     const shirtToAdd: Shirt = { sku: "9", color: "orange", size: "small" };
-    const shirtsObjectGraph = new ObjectGraph<Shirt>(shirtsMock, (shirt) => shirt.sku);
+    const shirtsGraph = new ObjectGraph<Shirt>(shirtsMock, (shirt) => shirt.sku);
 
-    expect(shirtsObjectGraph.size).toBe(8);
+    expect(shirtsGraph.size).toBe(8);
 
-    shirtsObjectGraph.add(shirtToAdd);
+    shirtsGraph.add(shirtToAdd);
 
-    expect(shirtsObjectGraph.size).toBe(9);
+    expect(shirtsGraph.size).toBe(9);
   });
 });
 
 describe("keys()", () => {
   test("gets an iterator object that contains the keys of the object graph", () => {
-    const shirtsObjectGraph = new ObjectGraph<Shirt>(shirtsMock, (shirt) => shirt.sku);
+    const shirtsGraph = new ObjectGraph<Shirt>(shirtsMock, (shirt) => shirt.sku);
 
-    const shirtsObjectGraphKeysIterator = shirtsObjectGraph.keys();
-    const shirtsObjectGraphKeys = Array.from(shirtsObjectGraphKeysIterator);
+    const shirtsGraphKeysIterator = shirtsGraph.keys();
+    const shirtsGraphKeys = Array.from(shirtsGraphKeysIterator);
 
-    expectTypeOf(shirtsObjectGraphKeysIterator[Symbol.iterator]).toBeFunction();
-    expect(shirtsObjectGraphKeys).toEqual(["1", "2", "3", "4", "5", "6", "7", "8"]);
+    expectTypeOf(shirtsGraphKeysIterator[Symbol.iterator]).toBeFunction();
+    expect(shirtsGraphKeys).toEqual(["1", "2", "3", "4", "5", "6", "7", "8"]);
   });
 });
 
 describe("values()", () => {
   test("gets an iterator object that contains the values of the object graph", () => {
-    const shirtsObjectGraph = new ObjectGraph<Shirt>(shirtsMock, (shirt) => shirt.sku);
+    const shirtsGraph = new ObjectGraph<Shirt>(shirtsMock, (shirt) => shirt.sku);
 
-    const shirtsObjectGraphValuesIterator = shirtsObjectGraph.values();
-    const shirtsObjectGraphValues = Array.from(shirtsObjectGraphValuesIterator);
+    const shirtsGraphValuesIterator = shirtsGraph.values();
+    const shirtsGraphValues = Array.from(shirtsGraphValuesIterator);
 
-    expectTypeOf(shirtsObjectGraphValuesIterator[Symbol.iterator]).toBeFunction();
-    expect(shirtsObjectGraphValues).toHaveLength(8);
-    expect(shirtsObjectGraphValues[0]).toStrictEqual(shirtsMock[0]);
+    expectTypeOf(shirtsGraphValuesIterator[Symbol.iterator]).toBeFunction();
+    expect(shirtsGraphValues).toHaveLength(8);
+    expect(shirtsGraphValues[0]).toStrictEqual(shirtsMock[0]);
   });
 });
 
 describe("get()", () => {
   test("logs an error when there is no node with the provided key in the object graph", () => {
     const consoleErrorSpy = vi.spyOn(console, "error");
-    const shirtsObjectGraph = new ObjectGraph<Shirt>(shirtsMock, (shirt) => shirt.sku);
+    const shirtsGraph = new ObjectGraph<Shirt>(shirtsMock, (shirt) => shirt.sku);
 
-    const returnedNode = shirtsObjectGraph.get("9");
+    const returnedNode = shirtsGraph.get("9");
 
     expect(consoleErrorSpy).toHaveBeenCalled();
     expect(returnedNode).toBeUndefined();
   });
 
   test("gets a node of the object graph", () => {
-    const shirtsObjectGraph = new ObjectGraph<Shirt>(shirtsMock, (shirt) => shirt.sku);
+    const shirtsGraph = new ObjectGraph<Shirt>(shirtsMock, (shirt) => shirt.sku);
 
-    const returnedNode = shirtsObjectGraph.get(shirtsMock[0].sku);
+    const returnedNode = shirtsGraph.get(shirtsMock[0].sku);
 
     expect(returnedNode?.color).toBe(shirtsMock[0].color);
     expect(returnedNode?.size).toBe(shirtsMock[0].size);
@@ -82,59 +82,59 @@ describe("get()", () => {
 
 describe("copy()", () => {
   test("gets a copy of the original object graph", () => {
-    const shirtsObjectGraph = new ObjectGraph<Shirt>(shirtsMock, (shirt) => shirt.sku);
+    const shirtsGraph = new ObjectGraph<Shirt>(shirtsMock, (shirt) => shirt.sku);
 
-    const copiedShirtsObjectGraph = shirtsObjectGraph.copy();
+    const copiedShirtsGraph = shirtsGraph.copy();
 
-    expect(shirtsObjectGraph.get("1")).toEqual(copiedShirtsObjectGraph.get("1"));
+    expect(shirtsGraph.get("1")).toEqual(copiedShirtsGraph.get("1"));
   });
 });
 
 describe("subgraph()", () => {
   test("gets a subgraph of the original object graph", () => {
-    const shirtsObjectGraph = new ObjectGraph<Shirt>(shirtsMock, (shirt) => shirt.sku);
-    const shirtsObjectSubgraph = shirtsObjectGraph.subgraph(["1", "2", "3", "4"]);
+    const shirtsGraph = new ObjectGraph<Shirt>(shirtsMock, (shirt) => shirt.sku);
+    const shirtsSubgraph = shirtsGraph.subgraph(["1", "2", "3", "4"]);
 
-    expect(shirtsObjectGraph).toHaveLength(8);
-    expect(shirtsObjectSubgraph).toHaveLength(4);
+    expect(shirtsGraph).toHaveLength(8);
+    expect(shirtsSubgraph).toHaveLength(4);
   });
 });
 
 describe("add()", () => {
   test("logs an error when a node with the same key already exists in the object graph", () => {
     const consoleErrorSpy = vi.spyOn(console, "error");
-    const shirtsObjectGraph = new ObjectGraph<Shirt>(shirtsMock, (shirt) => shirt.sku);
+    const shirtsGraph = new ObjectGraph<Shirt>(shirtsMock, (shirt) => shirt.sku);
 
-    shirtsObjectGraph.add(shirtsMock[0]);
+    shirtsGraph.add(shirtsMock[0]);
 
     expect(consoleErrorSpy).toHaveBeenCalled();
   });
 
   test("adds a node to the object graph", () => {
-    const shirtsObjectGraph = new ObjectGraph<Shirt>([], (shirt) => shirt.sku);
+    const shirtsGraph = new ObjectGraph<Shirt>([], (shirt) => shirt.sku);
 
-    shirtsObjectGraph.add(shirtsMock[0]);
+    shirtsGraph.add(shirtsMock[0]);
 
-    expect(shirtsObjectGraph.get("1")).toEqual(shirtsMock[0]);
+    expect(shirtsGraph.get("1")).toEqual(shirtsMock[0]);
   });
 });
 
 describe("toAdded()", () => {
   test("gets a copy of the original object graph with a received node added", () => {
     const consoleErrorSpy = vi.spyOn(console, "error");
-    const shirtsObjectGraph = new ObjectGraph<Shirt>([], (shirt) => shirt.sku);
+    const shirtsGraph = new ObjectGraph<Shirt>([], (shirt) => shirt.sku);
 
-    const copiedShirtsObjectGraph = shirtsObjectGraph.toAdded(shirtsMock[0]);
+    const copiedShirtsGraph = shirtsGraph.toAdded(shirtsMock[0]);
 
-    expect(shirtsObjectGraph.size).toBe(0);
-    expect(copiedShirtsObjectGraph.size).toBe(1);
+    expect(shirtsGraph.size).toBe(0);
+    expect(copiedShirtsGraph.size).toBe(1);
 
-    const returnedNodeFromCopy = copiedShirtsObjectGraph.get("1");
+    const returnedNodeFromCopy = copiedShirtsGraph.get("1");
 
     expect(consoleErrorSpy).not.toHaveBeenCalled();
     expect(returnedNodeFromCopy).toBeDefined();
 
-    const returnedNodeFromOriginal = shirtsObjectGraph.get("1");
+    const returnedNodeFromOriginal = shirtsGraph.get("1");
 
     expect(consoleErrorSpy).toHaveBeenCalled();
     expect(returnedNodeFromOriginal).toBeUndefined();
@@ -144,43 +144,43 @@ describe("toAdded()", () => {
 describe("update()", () => {
   test("logs an error when there is no node with the same key in the object graph", () => {
     const consoleErrorSpy = vi.spyOn(console, "error");
-    const shirtsObjectGraph = new ObjectGraph<Shirt>(shirtsMock, (shirt) => shirt.sku);
+    const shirtsGraph = new ObjectGraph<Shirt>(shirtsMock, (shirt) => shirt.sku);
 
-    shirtsObjectGraph.update(extraShirtsMock[0]);
+    shirtsGraph.update(extraShirtsMock[0]);
 
     expect(consoleErrorSpy).toHaveBeenCalled();
   });
 
   test("updates a node in the object graph", () => {
     const shirtToUpdate: Shirt = { sku: "1", color: "red", size: "large" };
-    const shirtsObjectGraph = new ObjectGraph<Shirt>(shirtsMock, (shirt) => shirt.sku);
+    const shirtsGraph = new ObjectGraph<Shirt>(shirtsMock, (shirt) => shirt.sku);
 
-    expect(shirtsObjectGraph.get("1")?.size).toBe("small");
+    expect(shirtsGraph.get("1")?.size).toBe("small");
 
-    shirtsObjectGraph.update(shirtToUpdate);
+    shirtsGraph.update(shirtToUpdate);
 
-    expect(shirtsObjectGraph.get("1")?.size).toBe("large");
+    expect(shirtsGraph.get("1")?.size).toBe("large");
   });
 });
 
 describe("toUpdated()", () => {
   test("gets a copy of the original object graph with a received node updated", () => {
     const shirtToUpdate: Shirt = { sku: "1", color: "red", size: "large" };
-    const shirtsObjectGraph = new ObjectGraph<Shirt>(shirtsMock, (shirt) => shirt.sku);
+    const shirtsGraph = new ObjectGraph<Shirt>(shirtsMock, (shirt) => shirt.sku);
 
-    const copiedShirtsObjectGraph = shirtsObjectGraph.toUpdated(shirtToUpdate);
+    const copiedShirtsGraph = shirtsGraph.toUpdated(shirtToUpdate);
 
-    expect(shirtsObjectGraph.get("1")?.size).toBe("small");
-    expect(copiedShirtsObjectGraph.get("1")?.size).toBe("large");
+    expect(shirtsGraph.get("1")?.size).toBe("small");
+    expect(copiedShirtsGraph.get("1")?.size).toBe("large");
   });
 });
 
 describe("remove()", () => {
   test("logs an error when there is no node with the provided key in the object graph", () => {
     const consoleErrorSpy = vi.spyOn(console, "error");
-    const shirtsObjectGraph = new ObjectGraph<Shirt>(shirtsMock, (shirt) => shirt.sku);
+    const shirtsGraph = new ObjectGraph<Shirt>(shirtsMock, (shirt) => shirt.sku);
 
-    const returnedNode = shirtsObjectGraph.remove("9");
+    const returnedNode = shirtsGraph.remove("9");
 
     expect(consoleErrorSpy).toHaveBeenCalled();
     expect(returnedNode).toBeUndefined();
@@ -188,18 +188,18 @@ describe("remove()", () => {
 
   test("removes a node from the object graph", () => {
     const consoleErrorSpy = vi.spyOn(console, "error");
-    const shirtsObjectGraph = new ObjectGraph<Shirt>(shirtsMock, (shirt) => shirt.sku);
+    const shirtsGraph = new ObjectGraph<Shirt>(shirtsMock, (shirt) => shirt.sku);
 
-    const returnedNodeFromFirstAttempt = shirtsObjectGraph.get("1");
+    const returnedNodeFromFirstAttempt = shirtsGraph.get("1");
 
     expect(consoleErrorSpy).not.toHaveBeenCalled();
     expect(returnedNodeFromFirstAttempt).toBeDefined();
 
-    shirtsObjectGraph.remove("1");
+    shirtsGraph.remove("1");
 
     expect(consoleErrorSpy).not.toHaveBeenCalled();
 
-    const returnedNodeFromSecondAttempt = shirtsObjectGraph.get("1");
+    const returnedNodeFromSecondAttempt = shirtsGraph.get("1");
 
     expect(consoleErrorSpy).toHaveBeenCalled();
     expect(returnedNodeFromSecondAttempt).toBeUndefined();
@@ -209,19 +209,19 @@ describe("remove()", () => {
 describe("toRemoved()", () => {
   test("gets a copy of the original object graph with a received node removed", () => {
     const consoleErrorSpy = vi.spyOn(console, "error");
-    const shirtsObjectGraph = new ObjectGraph<Shirt>(shirtsMock, (shirt) => shirt.sku);
+    const shirtsGraph = new ObjectGraph<Shirt>(shirtsMock, (shirt) => shirt.sku);
 
-    const copiedShirtsObjectGraph = shirtsObjectGraph.toRemoved("1");
+    const copiedShirtsGraph = shirtsGraph.toRemoved("1");
 
-    expect(shirtsObjectGraph.size).toBe(8);
-    expect(copiedShirtsObjectGraph.size).toBe(7);
+    expect(shirtsGraph.size).toBe(8);
+    expect(copiedShirtsGraph.size).toBe(7);
 
-    const returnedNodeFromOriginal = shirtsObjectGraph.get("1");
+    const returnedNodeFromOriginal = shirtsGraph.get("1");
 
     expect(consoleErrorSpy).not.toHaveBeenCalled();
     expect(returnedNodeFromOriginal).toBeDefined();
 
-    const returnedNodeFromCopy = copiedShirtsObjectGraph.get("1");
+    const returnedNodeFromCopy = copiedShirtsGraph.get("1");
 
     expect(consoleErrorSpy).toHaveBeenCalled();
     expect(returnedNodeFromCopy).toBeUndefined();
@@ -230,9 +230,9 @@ describe("toRemoved()", () => {
 
 describe("valuesOf()", () => {
   test("gets all values of the provided property", () => {
-    const shirtsObjectGraph = new ObjectGraph<Shirt>(shirtsMock, (shirt) => shirt.sku);
+    const shirtsGraph = new ObjectGraph<Shirt>(shirtsMock, (shirt) => shirt.sku);
 
-    const sizePropertyValues = shirtsObjectGraph.valuesOf("size");
+    const sizePropertyValues = shirtsGraph.valuesOf("size");
 
     expect(sizePropertyValues).toContain("small");
     expect(sizePropertyValues).toContain("medium");
@@ -240,9 +240,9 @@ describe("valuesOf()", () => {
   });
 
   test("gets all values of the provided property from selected nodes", () => {
-    const shirtsObjectGraph = new ObjectGraph<Shirt>(shirtsMock, (shirt) => shirt.sku);
+    const shirtsGraph = new ObjectGraph<Shirt>(shirtsMock, (shirt) => shirt.sku);
 
-    const sizePropertyValues = shirtsObjectGraph.valuesOf("size", ["1", "2", "3", "4"]);
+    const sizePropertyValues = shirtsGraph.valuesOf("size", ["1", "2", "3", "4"]);
 
     expect(sizePropertyValues).toContain("small");
     expect(sizePropertyValues).toContain("medium");
@@ -254,9 +254,9 @@ describe("match()", () => {
   test("gets all nodes that match with the provided shape", () => {
     const colorsToMatch: Color[] = ["red", "blue"];
     const sizesToMatch: Size[] = ["small", "medium"];
-    const shirtsObjectGraph = new ObjectGraph<Shirt>(shirtsMock, (shirt) => shirt.sku);
+    const shirtsGraph = new ObjectGraph<Shirt>(shirtsMock, (shirt) => shirt.sku);
 
-    const matchedShirts = shirtsObjectGraph.match({
+    const matchedShirts = shirtsGraph.match({
       color: colorsToMatch,
       size: sizesToMatch,
     });
@@ -270,9 +270,9 @@ describe("match()", () => {
 
   test("gets matched nodes ignoring undefined value in the shape", () => {
     const colorsToMatch: Color[] = ["yellow", "green"];
-    const shirtsObjectGraph = new ObjectGraph<Shirt>(shirtsMock, (shirt) => shirt.sku);
+    const shirtsGraph = new ObjectGraph<Shirt>(shirtsMock, (shirt) => shirt.sku);
 
-    const matchedShirts = shirtsObjectGraph.match({
+    const matchedShirts = shirtsGraph.match({
       color: colorsToMatch,
       size: undefined,
     });
@@ -284,9 +284,9 @@ describe("match()", () => {
   });
 
   test("gets all nodes for an empty shape", () => {
-    const shirtsObjectGraph = new ObjectGraph<Shirt>(shirtsMock, (shirt) => shirt.sku);
+    const shirtsGraph = new ObjectGraph<Shirt>(shirtsMock, (shirt) => shirt.sku);
 
-    const matchedShirts = shirtsObjectGraph.match({});
+    const matchedShirts = shirtsGraph.match({});
 
     expect(matchedShirts).toHaveLength(8);
   });
