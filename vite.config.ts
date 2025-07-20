eee0082a9a03e1d648047eb5865bad3cd2b8fadd
Lucas Alves Costa <50385918+luckasnix@users.noreply.github.com>
2025-07-20 09:16:02 -0300

    chore: upgrade Biome to version 2 (#87)

--- vite.config.ts
+++ vite.config.ts
@@ -3,24 +3,24 @@ import { defineConfig } from "vite";
 import dts from "vite-plugin-dts";
 
 const viteConfig = defineConfig({
-  build: {
-    lib: {
-      entry: resolve(__dirname, "src/index.ts"),
-      formats: ["es"],
-      fileName: "index",
-    },
-    rollupOptions: {
-      output: {
-        preserveModules: true,
-        entryFileNames: "[name].js",
-      },
-    },
-  },
-  plugins: [
-    dts({
-      include: ["src"],
-    }),
-  ],
+	build: {
+		lib: {
+			entry: resolve(__dirname, "src/index.ts"),
+			formats: ["es"],
+			fileName: "index",
+		},
+		rollupOptions: {
+			output: {
+				preserveModules: true,
+				entryFileNames: "[name].js",
+			},
+		},
+	},
+	plugins: [
+		dts({
+			include: ["src"],
+		}),
+	],
 });
 
 export default viteConfig;
